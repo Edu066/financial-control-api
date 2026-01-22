60489a20ae924e136d64e4bbef8f03ac921c783e
Edu066 <o.eduardo@aluno.ufabc.edu.br>
2026-01-22 12:53:03 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,18 @@
+import sqlite3
+
+def connect_db():
+    return sqlite3.connect("finance.db")
+
+def create_table():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS transactions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            type TEXT,
+            amount REAL
+        )
+    """)
+    conn.commit()
+    conn.close()
 
